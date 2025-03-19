fe14aab7e01e98ef8f8b39ac7adc4f0a79dc3e0a
abdalla mansour <a.mansour.code@gmail.com>
2025-03-19 17:59:29 +0200

    Make about us section

--- src/app/(all)/about-us/OurVision.tsx
+++ src/app/(all)/about-us/OurVision.tsx
@@ -1,22 +1,57 @@
+"use client"
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./style.css"
+import { useTranslations, useLocale } from "next-intl";
+
 import { message_icon, view_icon } from "@/assets";
-import { useTranslations } from "next-intl";
 import Image from "next/image";
-import React from "react";
 
 const OurVision = () => {
   const t = useTranslations();
-  const data = [
+  const locale = useLocale();
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/about-us`, {
+        headers: {
+          "Accept-Language": locale,
+        },
+      })
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("Error fetching data:", err);
+        setLoading(false);
+      });
+  }, [locale]);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <p className="text-2xl font-semibold text-gray-500">{t("Loading")}</p>
+      </div>
+    );
+  }
+
+  const about = [
     {
       icon: message_icon,
       title: t("the message"),
-      description: t("the message desc"),
+      description: data?.our_message,
     },
     {
       icon: view_icon,
       title: t("The vision"),
-      description: t("The vision desc"),
+      description: data?.our_vision,
     },
   ];
+
   return (
     <section className="py-10 md:py-20">
       <div className="container">
@@ -24,15 +59,15 @@ const OurVision = () => {
           <div className="text-2xl lg:text-5xl font-bold text-center">
             {t("Our vision and mission")}
           </div>
-          {data.map((item,idx) => (
+          {about.map((item, idx) => (
             <div className="flex items-center bg-[#E2AE6352] rounded-[36px] p-4 md:p-5 gap-4 md:gap-8" key={idx}>
-                <div className="flex-c min-w-[45px]">
-                    <Image src={item.icon} alt="icon" loading="lazy" />
-                </div>
-                <div className="flex flex-col">
-                    <span className="font-bold text-xl md:text-2xl">{item.title}</span>
-                    <span>{item.description}</span>
-                </div>
+              <div className="flex-c min-w-[45px]">
+                <Image src={item.icon} alt="icon" loading="lazy" />
+              </div>
+              <div className="flex flex-col">
+                <span className="font-bold text-xl md:text-2xl">{item.title}</span>
+                <span>{item.description}</span>
+              </div>
             </div>
           ))}
         </div>
