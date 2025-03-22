ca6b606cc4d5ef24b083c4edfa8c80f5b6c87a8d
abdalla mansour <a.mansour.code@gmail.com>
2025-03-22 16:54:00 +0200

    Make Team member section

--- src/app/(all)/work-team/page.tsx
+++ src/app/(all)/work-team/page.tsx
@@ -1,21 +1,52 @@
-"use client";
-import React, { useState } from "react";
-import { useTranslations } from "next-intl";
+"use client"
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+// import "./style.css"
+import { useTranslations, useLocale } from "next-intl";
 import HaveQuestion from "@/components/Question/HaveQuestion";
 import Member from "@/components/Member/Member";
 
 const WorkTeamPage = () => {
-  
   const t = useTranslations();
-  const onsubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-  };
+  const locale = useLocale();
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/workteam`, {
+        headers: {
+          "Accept-Language": locale,
+        },
+      })
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("Error fetching data:", err);
+        setLoading(false);
+      });
+  }, [locale]);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <p className="text-2xl font-semibold text-gray-500">{t("Loading")}</p>
+      </div>
+    );
+  }
+
+  let team = data?.data;
+
+
   return (
     <section className="px-12 bg-[#FAFAFA]">
       <div className="container">
         <div className="mx-auto grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6 gap-3">
-        {[...Array(8)].map((_, index) => (
-          <Member/>
+        {team?.map((member, index) => (
+          <Member member={member} />
         ))}
         </div>
         <div className="mb-16">
--- src/components/Member/Member.tsx
+++ src/components/Member/Member.tsx
@@ -1,26 +1,30 @@
-import { facebook_icon, member_user_avater, persone, snapchat_icon, whatsapp_icon } from "@/assets";
-import { useTranslations } from "next-intl";
+import { facebook_icon, snapchat_icon, whatsapp_icon } from "@/assets";
 import Image from "next/image";
 import React from "react";
-import "./style.css"
+// import "./style.css"
 
-const Member = () => {
-  const t = useTranslations();
-  const arr = new Array(8).fill("");
+const Member = ({ member }) => {
 
   return (
     <div className="bg-white member text-center rounded-xl p-4 pt-8">
-      <Image src={member_user_avater} alt="Profile" className="rounded-full w-32 h-32 mx-auto" />
-      <h2 className="text-xl" style={{ fontWeight: "bolder !important", marginBottom: "5px" }}>محمد جوبان</h2>
-      <p className="text-gray-600">مؤسس المنصة</p>
+      <Image
+        src={member.image}
+        width={128}
+        height={128}
+        alt="Profile"
+        className="rounded-full mx-auto"
+      />
+
+      <h2 className="text-xl" style={{ fontWeight: "bolder !important", marginBottom: "5px" }}>{member.name}</h2>
+      <p className="text-gray-600">{member.job_title}</p>
       <div className="flex justify-center mt-4 space-x-4 mt-6">
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+        <a href={member.facebook} target="_blank" rel="noopener noreferrer">
           <Image src={facebook_icon} alt="Facebook" className="w-6 h-6" />
         </a>
-        <a href="https://snapchat.com" target="_blank" rel="noopener noreferrer">
+        <a href={member.snapchat} target="_blank" rel="noopener noreferrer">
           <Image src={snapchat_icon} alt="Snapchat" className="w-6 h-6" />
         </a>
-        <a href="https://whatsapp.com" target="_blank" rel="noopener noreferrer">
+        <a href={member.whatsapp} target="_blank" rel="noopener noreferrer">
           <Image src={whatsapp_icon} alt="WhatsApp" className="w-6 h-6" />
         </a>
       </div>
