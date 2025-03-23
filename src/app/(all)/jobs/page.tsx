cae16570d0233f6ee5527d8ce5a6f77ffbb1cd0a
abdalla mansour <a.mansour.code@gmail.com>
2025-03-23 21:53:29 +0200

    make job section

--- src/app/(all)/jobs/page.tsx
+++ src/app/(all)/jobs/page.tsx
@@ -1,23 +1,52 @@
-"use client";
-import React, { useState } from "react";
-import { useTranslations } from "next-intl";
+"use client"
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useTranslations, useLocale } from "next-intl";
 import HaveQuestion from "@/components/Question/HaveQuestion";
-import Manager from "@/components/Manager/Manager";
 import Job from "@/components/Job/Job";
 
 const JobsPage = () => {
-  
   const t = useTranslations();
-  const onsubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-  };
+
+  const locale = useLocale();
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/jobs`, {
+        headers: {
+          "Accept-Language": locale,
+        },
+      })
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("Error fetching data:", err);
+        setLoading(false);
+      });
+  }, [locale]);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <p className="text-2xl font-semibold text-gray-500">{t("Loading")}</p>
+      </div>
+    );
+  }
+
+  let jobs = data?.data;
+
   return (
     <section className="px-12 bg-[#FAFAFA]">
       <div className="container">
         <div className="mx-auto grid md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 mt-6 gap-6">
-        {[...Array(6)].map((_, index) => (
-          <Job/>
-        ))}
+          {jobs?.map((job, index) => (
+            <Job key={index} job={job} />
+          ))}
         </div>
         <div className="mb-16">
           <HaveQuestion />
--- src/components/Job/Job.tsx
+++ src/components/Job/Job.tsx
@@ -1,20 +1,19 @@
-import { job_image } from "@/assets";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import React from "react";
 import "./style.css"
 
-const Job = () => {
+const Job = ({ job }) => {
   const t = useTranslations();
   const arr = new Array(8).fill("");
 
   return (
-    <div className="flex items-center  rounded-xl w-full h-full  job">
+    <div className="flex items-center  rounded-xl w-full h-full job">
       <div className="">
-        <Image src={job_image} alt="Profile" className="w-full h-full max-h-[128px] max-w-[168px] mr-[-10px]" />
+        <Image src={job.image} alt="Profile" width={168} height={128} className="w-full h-full max-h-[128px] max-w-[168px] mr-[-10px]" />
       </div>
       <div className="flex-grow px-6">
-        <h2 className="text-xl font-bold mb-2 ml-6">مدير موقع الإلكترونى</h2>
+        <h2 className="text-xl font-bold mb-2 ml-6">{job.title}</h2>
         <button className=" bg-[#E2AE63] text-black px-8 cursor-pointer  float-left rounded">{t("Submit Now")}</button>
       </div>
     </div>
