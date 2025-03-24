9cf15a36bc2f24d91c0f53bdfc151d13b293f9c0
abdalla mansour <a.mansour.code@gmail.com>
2025-03-24 17:18:00 +0200

    Make Managers section

--- src/app/(all)/managers/page.tsx
+++ src/app/(all)/managers/page.tsx
@@ -1,21 +1,50 @@
-"use client";
-import React, { useState } from "react";
-import { useTranslations } from "next-intl";
+"use client"
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useTranslations, useLocale } from "next-intl";
 import HaveQuestion from "@/components/Question/HaveQuestion";
 import Manager from "@/components/Manager/Manager";
 
 const ManagerPage = () => {
-  
+
   const t = useTranslations();
-  const onsubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-  };
+  const locale = useLocale();
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/admins`, {
+        headers: {
+          "Accept-Language": locale,
+        },
+      })
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("Error fetching data:", err);
+        setLoading(false);
+      });
+  }, [locale]);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <p className="text-2xl font-semibold text-gray-500">{t("Loading")}</p>
+      </div>
+    );
+  }
+
+  let admins = data?.data;
   return (
     <section className="px-12 bg-[#FAFAFA]">
       <div className="container">
         <div className="mx-auto grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6 gap-3">
-        {[...Array(4)].map((_, index) => (
-          <Manager/>
+        {admins?.map((admin, index) => (
+          <Manager key={index} admin={admin} />
         ))}
         </div>
         <div className="mb-16">
--- src/components/Manager/Manager.tsx
+++ src/components/Manager/Manager.tsx
@@ -4,23 +4,27 @@ import Image from "next/image";
 import React from "react";
 import "./style.css"
 
-const Manager = () => {
-  const t = useTranslations();
-  const arr = new Array(8).fill("");
+const Manager = ({ admin }) => {
 
   return (
     <div className=" text-center rounded-xl p-4 pt-8">
-      <Image src={member_user_avater} alt="Profile" className="rounded-full w-32 h-32 mx-auto" />
-      <h2 className="text-xl" style={{ fontWeight: "bolder !important", marginBottom: "5px" }}>محمد جوبان</h2>
-      <p className="text-gray-600">مؤسس المنصة</p>
+      <Image
+        src={admin.image}
+        width={128}
+        height={128}
+        alt="Profile"
+        className="rounded-full mx-auto"
+      />
+      <h2 className="text-xl" style={{ fontWeight: "bolder !important", marginBottom: "5px" }}>{admin.name}</h2>
+      <p className="text-gray-600">{admin.job_title}</p>
       <div className="flex justify-center mt-4 space-x-4 mt-6">
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+        <a href={admin.facebook} target="_blank" rel="noopener noreferrer">
           <Image src={facebook_icon} alt="Facebook" className="w-6 h-6" />
         </a>
-        <a href="https://snapchat.com" target="_blank" rel="noopener noreferrer">
+        <a href={admin.snapchat} target="_blank" rel="noopener noreferrer">
           <Image src={snapchat_icon} alt="Snapchat" className="w-6 h-6" />
         </a>
-        <a href="https://whatsapp.com" target="_blank" rel="noopener noreferrer">
+        <a href={admin.whatsapp} target="_blank" rel="noopener noreferrer">
           <Image src={whatsapp_icon} alt="WhatsApp" className="w-6 h-6" />
         </a>
       </div>
